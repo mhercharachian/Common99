fbefd685189b895a5eeb76c2e7c063c416d5a85d
Matthewdonut <mileto.matthew@gmail.com>
2016-09-23 10:18:55 -0400

    Update Name.java

--- Name.java
+++ Name.java
@@ -1,168 +1,63 @@
 /**
  * 
  */
-package dw317.lib;
+package dw317.lib.creditcard;
 
 /**
  * @author Matt, Mher, Tahar
- *
+ * 
+ * This program acts as a single Visa, which extends from the AbstractCreditCard class.
+ * 
  */
-public class Name {
-
-	private String firstName;
-	private String lastName;
-
-	/**
-	 * Default constructor
-	 */
-
-	public Name() {
-		this.firstName = "";
-		this.lastName = "";
-	}
-
+public class Visa extends AbstractCreditCard {
+	
 	/**
 	 * 
-	 * 2nd constructor
-	 * 
-	 * @param the
-	 *            First name
-	 * @param the
-	 *            Last name
+	 * @param number: the number of the Visa, once it's been validated
+	 * @throws IllegalArgumentException if the Visa number is invalid
+	 * 	
 	 */
 
-	public Name(String firstName, String lastName) {
-		setFirstName(firstName);
-		setLastName(lastName);
-	}
+	private static final long serialVersionUID = 42031766871L;
 
-	/**
-	 * 
-	 * @return the first name
-	 */
-
-	public String getFirstName() {
-		return this.firstName;
-	}
-
-	/**
-	 * 
-	 * Sets the first name, once validated
-	 * 
-	 * @param firstName
-	 */
-
-	private void setFirstName(String firstName) {
-		this.firstName = validateName(firstName);
-	}
-
-	/**
-	 * 
-	 * @return the last name
-	 */
-
-	public String getLastName() {
-		return this.lastName;
-	}
-
-	/**
-	 * 
-	 * Sets the last name, once validated
-	 * 
-	 * @param lastName
-	 */
-
-	private void setLastName(String lastName) {
-		this.lastName = validateName(lastName);
-	}
-
-	/**
-	 * 
-	 * Overrides the toString class so a full name looks like: first name
-	 * (space) last name
-	 * 
-	 */
-
-	@Override
-	public String toString() {
-		return this.firstName + " " + this.lastName;
-	}
-
-	/**
-	 * 
-	 * Validates the first and last name. The names are invalid if:
-	 * 
-	 * 	- They consist of less than 2 characters
-	 * 	- They consist of a characters that aren't a 
-	 *    letter, apostrophe('), hyphen(-) or space
-	 *  - They consist of an apostrophe, hyphen or space
-	 *    that aren't between two letters
-	 * 
-	 * 
-	 * @param name
-	 * @return the name, if valid
-	 * @throws IllegalArgumentException if the name is invalid
-	 */
-
-	private static String validateName(String name) throws IllegalArgumentException {
-
-		name = name.trim();
-		if (name == null)
-			throw new IllegalArgumentException("Last name cannot be null");
-
-		if (name.isEmpty())
-			throw new IllegalArgumentException("Last name cannot be empty");
-
-		if (name.length() < 2)
-			throw new IllegalArgumentException("Last name must be at least 2 characters");
-
-		char c;
-		int length = name.length();
-
-		for (int i = length - 1; i >= 0; i--) {
-			c = name.charAt(i);
-
-			// if char is not a letter nor a legal character, throw an exception
-			if (!Character.isLetter(c) && !isLegalChar(c))
-				throw new IllegalArgumentException("First or last name cannot contain " + name.charAt(i));
-
-			// before validating that the chars are legal, make sure we don't
-			// get an IndexOutOfBounds error
-			if (name.charAt(i) > 0 && name.charAt(i) < length-1)
-				// if the char is a legal character (but not a letter), check to
-				// see if the chars next to it are letters. If they are not, throw an exception
-				if (isLegalChar(c))
-					if (Character.isLetter((name.charAt(i - 1))) && Character.isLetter((name.charAt(i + 1))))
-						continue;
-					else
-						throw new IllegalArgumentException(c + " must be in between two letters");
-
-			// if the length is 2, then an exception will be thrown if
-			// one of the two characters are a legal character (but not a letter)
-			if (length == 2)
-				if (!isLegalChar(c) || !isLegalChar(name.charAt(i - 1)))
-					throw new IllegalArgumentException(c + " must be in between two letters");
-		}
-		return name;
+	public Visa(String number) throws IllegalArgumentException {
+		super(CardType.VISA, validateNumber(number));
 	}
 	
 	/**
 	 * 
-	 * Validates if the char is legal in a name
-	 * 
-	 * @param c: the char being validated
-	 * @return true if the char is legal, false if not
+	 * @param number: the supposed number of the Visa
+	 * @return the number if it is valid, an IllegalArgumentException if it is invalid
+	 * @throws IllegalArgumentException if the Visa number is invalid:
+	 * 									- if the number is not 16-digits long
+	 * 									- if the first number is not a 4
 	 */
 
-	private static boolean isLegalChar(char c) {
-		if (c == '\'')
-			return true;
-		if (c == ' ')
-			return true;
-		if (c == '-')
-			return true;
+	private static String validateNumber(String number) throws IllegalArgumentException {
 
-		return false;
+		long num = 0;
+		int length = number.length();
+		
+		if (length != 16)
+			throw new IllegalArgumentException("Invalid amount of digits");
+
+		try {
+			num = Long.parseLong(number);
+		} catch (IllegalArgumentException iae) {
+			System.out.println("Card number must contain numbers ONLY");
+			System.exit(0);
+		}
+
+		if ((int)(num / (Math.pow(10, 15))) != 4) { // validate that the first number is 4, then cast to integer to avoid decimals
+			throw new IllegalArgumentException("First number must be 4");
+		}
+		return number;
+	}
+
+	@Override
+	public CardType getType() {
+		return this.cardType;
 	}
 
 }
+
