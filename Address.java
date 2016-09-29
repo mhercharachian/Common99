1af1a44b66f7298b31d2576185fd0183de341836
Matthewdonut <mileto.matthew@gmail.com>
2016-09-29 11:17:08 -0400

    Update Address.java

--- Address.java
+++ Address.java
@@ -2,9 +2,10 @@
 * Defines an Address type.
 */
 package dw317.lib;
+
 /**
-* @author Type your name
-*/
+ * @author Type your name
+ */
 public class Address {
 	private String city = "";
 	private String civicNumber = "";
@@ -12,76 +13,86 @@ public class Address {
 	private String code = "";
 	private String streetName = "";
 
-
- public Address() { }
- public Address(String civicNumber, String streetName, String city) {
-		this.civicNumber = validateExistence("civic number", civicNumber);
-		this.streetName = validateExistence("street name", streetName);
-		this.city = validateExistence("city", city);
+	public Address() {
 	}
- public Address(Address address) {
-	 this.civicNumber = address.civicNumber;
-	 this.streetName = address.streetName;
-	 this.city = address.city;
- }
 
- public String getAddress() {
-	 String address = civicNumber + " " + streetName + "\n" + city;
-		address += (province.equals("")?"": (", " + province)) +
-			 (code.equals("")?"":("\n" + code));
+	public Address(String civicNumber, String streetName, String city) {
+		this.civicNumber = validateExistence("civic number", new String(civicNumber));
+		this.streetName = validateExistence("street name", new String(streetName));
+		this.city = validateExistence("city", new String(city));
+	}
+
+	public Address(Address address) {
+		this.civicNumber = address.getCivicNumber();
+		this.streetName = address.getStreetName();
+		this.city = address.getCity();
+	}
+
+	public String getAddress() {
+		String address = civicNumber + " " + streetName + "\n" + city;
+		address += (province.equals("") ? "" : (", " + province)) + (code.equals("") ? "" : ("\n" + code));
 
 		return address;
 
- }
-public String getCity() {
-	return city;
-}
-public void setCity(String city) {
-	this.city = validateExistence("city", city);
-}
-public String getCivicNumber() {
-	return civicNumber;
-}
-public void setCivicNumber(String civicNumber) {
-	this.civicNumber = validateExistence("civic number", civicNumber);
-}
-public String getProvince() {
-	return province;
-}
-public void setProvince(String province) {
-	this.province = province;
-}
-public String getCode() {
-	return code;
-}
-public void setCode(String code) {
-	this.code = code;
-}
-public String getStreetName() {
-	return streetName;
-}
-public void setStreetName(String streetName) {
-	this.streetName = validateExistence("street name", streetName);
-}
-public String toString() {
-		return (civicNumber + "*" +	streetName + "*" +
-				city + "*" +province + "*" +	code);
 	}
 
-private String validateExistence(String fieldName, String fieldValue) {
-	
-	if (fieldValue == null) {
-		throw new IllegalArgumentException("Address Error - " + fieldName
-			+ " must exist. Invalid value = " + fieldValue);
+	public String getCity() {
+		return new String(city);
 	}
-	String trimmedString = fieldValue.trim();
-	if (trimmedString.isEmpty()){
-		
-		throw new IllegalArgumentException("Address Error - " + fieldName
-			+ " must exist. Invalid value = " + fieldValue);
+
+	public void setCity(String city) {
+		this.city = validateExistence("city", city);
 	}
-	return trimmedString;
-}
+
+	public String getCivicNumber() {
+		return new String(civicNumber);
+	}
+
+	public void setCivicNumber(String civicNumber) {
+		this.civicNumber = validateExistence("civic number", civicNumber);
+	}
+
+	public String getProvince() {
+		return new String(province);
+	}
+
+	public void setProvince(String province) {
+		this.province = new String(province);
+	}
+
+	public String getCode() {
+		return new String(code);
+	}
+
+	public void setCode(String code) {
+		this.code = new String(code);
+	}
+
+	public String getStreetName() {
+		return new String(streetName);
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = validateExistence("street name", new String(streetName));
+	}
+
+	public String toString() {
+		return (civicNumber + "*" + streetName + "*" + city + "*" + province + "*" + code);
+	}
+
+	private String validateExistence(String fieldName, String fieldValue) {
+
+		if (fieldValue == null) {
+			throw new IllegalArgumentException(
+					"Address Error - " + fieldName + " must exist. Invalid value = " + fieldValue);
+		}
+		String trimmedString = fieldValue.trim();
+		if (trimmedString.isEmpty()) {
+
+			throw new IllegalArgumentException(
+					"Address Error - " + fieldName + " must exist. Invalid value = " + fieldValue);
+		}
+		return trimmedString;
+	}
 
 }
-
