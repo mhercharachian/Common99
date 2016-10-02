984be2491700e5989949780b62b85cb50f39d3e2
Speedst1998 <mtahar1998@gmail.com>
2016-10-02 15:27:05 -0400

    Add files via upload

--- HotelFactory.java
+++ HotelFactory.java
@@ -1,4 +1,22 @@
 
-public interface HotelFactory {
+public enum DawsonHotelFactory {
+	
+	package dw317.hotel.business.interfaces;
+
+import java.io.Serializable;
+import dw317.lib.creditcard.CreditCard;
+
+public interface HotelFactory extends Serializable {
+
+	Customer getCustomerInstance(String firstName, String lastName, String email);
+
+	CreditCard getCard(String cardtype, String number);
+
+	Room getRoomInstance(int roomNumber, String roomtype);
+
+	Reservation getReservationInstance(Customer aCustomer, Room aRoom, int inYear, int inMonth, int inDay, int outYear,
+			int outMonth, int outDay);
+
+	Reservation getReservationInstance(Reservation toCopy);
 
 }
